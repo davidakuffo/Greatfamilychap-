9bea95c6046659c97bfc850c0a691819a2dd32b3
davidoffei <david.akuffo11@gmail.com>
2026-03-24 18:30:40 +0000

    Event management system

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Contact_Us from './components/contact_Us'
 import Footer from './components/Footer'
 import Events from './components/Events'
 import Donate from './Donate'
+
 const App = () => {
 
   useEffect(() => {
